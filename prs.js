bc24b8873e366eb4791802948f992c02a45a15e1
Daryl Granger <darylgranger@gmail.com>
2018-08-26 16:04:17 -0700

    complete to intial Odin project standard.

--- prs.js
+++ prs.js
@@ -8,25 +8,24 @@ function generateComputerResult(){
 // 2 = scissors
 
 function battle(){
-    debugger;
     var playerChoice = parseInt(document.getElementById("player").value);
-    console.log("Player = " +playerChoice);
+        console.log("Player = " +playerChoice);
     var computerChoice = generateComputerResult();
-    console.log("Logic = " + computerChoice);
+        console.log("Logic = " + computerChoice);
 
- 
-    
     if (playerChoice === computerChoice) {
            console.log("Tie!");
        } else if (
            ( playerChoice === 0 && computerChoice === 1) || 
            ( playerChoice === 1 && computerChoice === 2) || 
-           (playerChoice === 2 && computerChoice ===0)){
+           ( playerChoice === 2 && computerChoice === 0)
+        ){
            console.log("You lose!");
        } else if (
            ( playerChoice === 0 && computerChoice === 2) || 
            ( playerChoice === 1 && computerChoice === 0) || 
-           ( playerChoice === 2 && computerChoice === 1)){
+           ( playerChoice === 2 && computerChoice === 1)
+        ){
            console.log("You win!");
        } else {
            console.log("You need to pick a weapon!")
