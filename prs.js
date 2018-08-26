5f29b220c65964db5f0ec7af924cf66d4b740b3c
Daryl Granger <darylgranger@gmail.com>
2018-08-26 13:15:49 -0700

    refactored logic to simplify and condense javascript.  Minor change to HTML to set values to numbers.

--- prs.js
+++ prs.js
@@ -3,9 +3,34 @@ function generateComputerResult(){
     return(computerResult);
 }
 
+// 0 = rock
+// 1 = paper
+// 2 = scissors
+
 function battle(){
-    var playerChoice = document.getElementById("player").value;
+    debugger;
+    var playerChoice = parseInt(document.getElementById("player").value);
+    console.log("Player = " +playerChoice);
     var computerChoice = generateComputerResult();
-    console.log(playerChoice);
-    console.log(computerChoice);
-}
+    console.log("Logic = " + computerChoice);
+
+ 
+    
+    if (playerChoice === computerChoice) {
+           console.log("Tie!");
+       } else if (
+           ( playerChoice === 0 && computerChoice === 1) || 
+           ( playerChoice === 1 && computerChoice === 2) || 
+           (playerChoice === 2 && computerChoice ===0)){
+           console.log("You lose!");
+       } else if (
+           ( playerChoice === 0 && computerChoice === 2) || 
+           ( playerChoice === 1 && computerChoice === 0) || 
+           ( playerChoice === 2 && computerChoice === 1)){
+           console.log("You win!");
+       } else {
+           console.log("You need to pick a weapon!")
+       };
+
+};
+
